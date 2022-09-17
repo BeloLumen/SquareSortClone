06e12ef106f3c4aaf4bc3ff5bc697cc440c89fb6
Mertcan ışık <56255420+BeloLumen@users.noreply.github.com>
2022-09-17 17:54:16 +0300

    düzenleme

--- SquareSortClone/Assets/Scripts/Cube.cs
+++ SquareSortClone/Assets/Scripts/Cube.cs
@@ -10,6 +10,7 @@ public class Cube: MonoBehaviour
     public Vector2 currentPos;
     public float moveSpeed;
     
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,12 @@ public class Cube: MonoBehaviour
         Swipe();
     }
    
+    public IEnumerator velocityZero()
+    {
+        yield return new WaitForSeconds(0.25f);
+        currentPos.Set(0, 0);
+        //currentPos.Normalize();
+    }
     private void Swipe()
     {
         
@@ -42,42 +49,50 @@ public class Cube: MonoBehaviour
         if (currentPos.y < 0 && currentPos.x < 0.5f && currentPos.x > -0.5f)
         {
             //down
-           // rb.velocity = Vector3.back * moveSpeed;
-            rb.AddForce(Vector3.back * moveSpeed);
+            rb.velocity = Vector3.back * moveSpeed;
+            //rb.AddForce(Vector3.back * moveSpeed);
         }
         else if (currentPos.y > 0 && currentPos.x < 0.5f && currentPos.x > -0.5f)
         {
             //up 
-            //rb.velocity = Vector3.forward * moveSpeed;
-            rb.AddForce(Vector3.forward * moveSpeed);
+            rb.velocity = Vector3.forward * moveSpeed;
+            //rb.AddForce(Vector3.forward * moveSpeed);
         }
         else if (currentPos.x < 0 && currentPos.y < 0.5f && currentPos.y > -0.5f)
         {
             // left 
-            //rb.velocity = Vector3.left * moveSpeed;
-            rb.AddForce(Vector3.left * moveSpeed);
+            rb.velocity = Vector3.left * moveSpeed;
+            //rb.AddForce(Vector3.left * moveSpeed);
         }
         else if (currentPos.x > 0 && currentPos.y < 0.5f && currentPos.y > -0.5f)
         {
             //right
-            //rb.velocity = Vector3.right * moveSpeed;
-            rb.AddForce(Vector3.right * moveSpeed);
+            rb.velocity = Vector3.right * moveSpeed;
+            //rb.AddForce(Vector3.right * moveSpeed);
         }
         else if (currentPos.x == 0 && currentPos.y == 0)
         {
+           
             rb.velocity = Vector3.zero;
+
         }
     }
     private void OnCollisionEnter(Collision collision)
     {   
-        if(collision.gameObject.tag != "Wall" && collision.gameObject.tag != "Cube")
-        {
-            currentPos.Set(0, 0);
-            currentPos.Normalize();
-        }
+       
         if(collision.gameObject.tag == "Wall" && collision.gameObject.GetComponent<MeshRenderer>().material.color== this.gameObject.GetComponent<MeshRenderer>().material.color)
         {
             Destroy(this.gameObject);
         }
+        if(collision.gameObject)
+        {
+            //currentPos.Set(0, 0);
+            StartCoroutine(velocityZero());
+        }
     }
+    
+
+
+
+
 }
